c34093bf43ad2280fa6ecc04dc8d3179386c5b20
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2022-12-22 19:11:41 +0300

    reload UserViewModel

--- ListExample/UserViewModel.swift
+++ ListExample/UserViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import Combine
+
+class UserViewModel: ObservableObject {
+    // 1
+    @Published var users: [User] = []
+    @Published var isRequestFailed = false
+    // 2
+    private let pageLimit = 25
+    private var currentLastId: Int? = nil
+  
+    private var cancellable: AnyCancellable?
+    
+    func getUsers() {
+        // 3
+        cancellable = APIService.shared.getUsers(perPage: pageLimit, sinceId: currentLastId)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    // 4
+                    self.isRequestFailed = true
+                    print(error)
+                case .finished:
+                    print("finished")
+                }
+            } receiveValue: { users in
+                // 5
+                self.users.append(contentsOf: users)
+                self.currentLastId = users.last?.id
+            }
+    }
+}
