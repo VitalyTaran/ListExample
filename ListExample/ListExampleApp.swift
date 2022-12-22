600c5d4c6550230c1dbfaaced53542bccacf6afd
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2022-12-22 18:17:45 +0300

    Initial Commit

---
+++ ListExample/ListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListExampleApp.swift
+//  ListExample
+//
+//  Created by Виталий Таран on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
