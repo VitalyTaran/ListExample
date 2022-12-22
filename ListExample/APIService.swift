a384f99b93e1408220e01d473269ab4a33bab73d
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2022-12-22 19:08:31 +0300

    reload APIService

--- ListExample/APIService.swift
+++ ListExample/APIService.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Combine
+
+class APIService {
+    static let shared = APIService()
+    // 1
+    func getUsers(perPage: Int = 30, sinceId: Int? = nil) -> AnyPublisher<[User], Error> {
+        // 2
+        var components = URLComponents(string: "https://api.github.com/users")!
+        components.queryItems = [
+            URLQueryItem(name: "per_page", value: "\(perPage)"),
+            URLQueryItem(name: "since", value: (sinceId != nil) ? "\(sinceId!)" : nil)
+        ]
+        // 3
+        let request = URLRequest(url: components.url!, timeoutInterval: 10)
+        // 4
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: [User].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
